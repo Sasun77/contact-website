d8dabaa3a6888248a31703357d5fc2bad7cc63be
Sasun77 <sasunsafaryan98@gmail.com>
2025-07-21 21:50:04 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
